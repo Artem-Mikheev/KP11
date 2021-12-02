d3055c53370900babcf470204e9fdeb74ff10874
Artem-Mikheev <95409239+Artem-Mikheev@users.noreply.github.com>
2021-12-02 11:57:55 +0300

    1

---
+++ Weather_AppMIH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather_App_MIKH
+//
+//  Created by user on 03.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
